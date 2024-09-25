65a7afc12ec3811135b4751c6ae6d52e655c98cf
akemashita <134128275+akemashita@users.noreply.github.com>
2024-09-25 14:23:00 +0000

    multi関数の追加

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 'use strict';
-function add(numbers) {
+
+function add(numbers_param) {
     let result = 0;
-    for (let num of numbers) {
+    for (let num of numbers_param) {
         result = result + num;
     }
     return result;
 }
+
+function multi(numbers_param) {
+    let result = 1;
+    for (let num of numbers_param) {
+        result = result * num;
+    }
+    return result;
+}
+
 module.exports = {
-    add: add
+    add: add,
+    multi: multi
 };
